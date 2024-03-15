36ee99eb32bf3c49bcaaba7420a59a13e24d00e9
hampus-code <hampusss96@gmail.com>
2024-03-15 14:02:00 +0100

    First Commit

---
+++ app/src/main/java/com/hampuscode/lektion_10_bonus_git/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.hampuscode.lektion_10_bonus_git
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Lektion_10_BONUS_Git"
+include(":app")
+ 
